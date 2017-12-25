325b14f04e76e245b736dde89dba694def4f579b
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-12-25 19:17:24 +0000

    CORE-1313 Add global event flag

--- src/Spryker/Zed/EventBehavior/EventBehaviorConfig.php
+++ src/Spryker/Zed/EventBehavior/EventBehaviorConfig.php
@@ -14,6 +14,8 @@ class EventBehaviorConfig extends AbstractBundleConfig
 {
     const EVENT_ENTITY_CHANGE_TIMEOUT_MINUTE = 5;
 
+    protected static $isEventDisabled = false;
+
     /**
      * @return int
      */
@@ -28,5 +30,29 @@ class EventBehaviorConfig extends AbstractBundleConfig
     public function getEventBehaviorTriggeringStatus()
     {
         return $this->get(EventBehaviorConstants::EVENT_BEHAVIOR_TRIGGERING_ACTIVE, false);
+    }
+
+    /**
+     * @return bool
+     */
+    public static function disableEvent()
+    {
+        return static::$isEventDisabled = true;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function enableEvent()
+    {
+        return static::$isEventDisabled = false;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isEventBehaviorDisabled()
+    {
+        return static::$isEventDisabled;
     }
 }
