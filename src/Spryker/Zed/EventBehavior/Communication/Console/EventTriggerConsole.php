fda5cc235dbbc37c8945abff8856d21b15bfa4b5
Olha Livitchuk <olha.livitchuk@spryker.com>
2026-02-16 19:41:04 +0100

    FRW-10644 CI Fixes

--- src/Spryker/Zed/EventBehavior/Communication/Console/EventTriggerConsole.php
+++ src/Spryker/Zed/EventBehavior/Communication/Console/EventTriggerConsole.php
@@ -98,8 +98,7 @@ class EventTriggerConsole extends Console
         if ($input->getOption(static::RESOURCE_IDS_OPTION)) {
             /** @var string $idsString */
             $idsString = $input->getOption(static::RESOURCE_IDS_OPTION);
-            /** @var list<string> $resourcesIds */
-            $resourcesIds = explode(',', $idsString);
+            $resourcesIds = array_map('intval', explode(',', $idsString));
         }
 
         $this->getFacade()->executeResolvedPluginsBySources($resources, $resourcesIds);
